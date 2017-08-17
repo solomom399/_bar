00b85be8b919bae15c32f12d8ce1db9147532a58
solomon <sobanjosodiq@gmail.com>
2017-08-17 12:28:01 -0700

    _bar

---
+++ js/custom.js
@@ -0,0 +1,175 @@
+$(document).ready(function () {
+
+	// var path = "https://savinglifesglobal.com.ng/lc_server/"
+	var path = "http://localhost/lc_server/"
+
+	var LC = function () {
+		this.user = JSON.parse(localStorage.getItem('user_details'))
+		this.displayUsersDetails = function () {
+			var user_details_object = JSON.parse(localStorage.getItem('user_details'))
+			$('.name').html('<i class="ti-user"></i> '+user_details_object.name)
+			
+			
+		}
+
+		this.load = function (text) {
+			swal({
+				title: '<div class="preloader-wrapper small active">'+
+					    '<div class="spinner-layer spinner-red-only">'+
+					      '<div class="circle-clipper left">'+
+					        '<div class="circle"></div>'+
+					      '</div><div class="gap-patch">'+
+					        '<div class="circle"></div>'+
+					      '</div><div class="circle-clipper right">'+
+					        '<div class="circle"></div>'+
+					      '</div>'+
+					    '</div>'+
+					  '</div>',
+				text: text,
+				html: true,
+				showConfirmButton : false
+			})
+		}
+
+
+		this.onBackKeyDown = function (e) {
+			e.preventDefault();
+		    var conf = confirm("Are you sure you want to exit this app")
+		    if(conf){
+		        navigator.app.exitApp();
+		    } else {
+		        alert('Enjoy yourself!');
+		    }
+		}
+
+
+		this.success = function (text, callback) {
+			swal({
+				title: '',
+				text: '<p>'+text+'</p>',
+				html: true,
+				type: 'success',
+				confirmButtonClass : 'btn blue'
+			},
+			function () {
+		        callback()
+		    })
+		}
+
+
+		this.warn = function (text, callback) {
+			swal({
+				title: '',
+				text: '<p>'+text+'</p>',
+				html: true,
+				type: 'warning',
+				confirmButtonClass : 'btn red darken-4'
+			},
+			function () {
+		        callback()
+		    })
+		}
+
+
+
+		this.makeUse = function (formData, file_name, callback, errorCallback = null, type = 'JSON') {
+			$.ajax({
+				url: path+file_name,
+				type: "POST",
+				data: formData,
+				cache: false,
+			    processData: false,
+			    contentType: false,
+			    dataType: type,
+				success: function(r) {
+					callback(r)
+				},
+			    error: function(XMLHttpRequest, textStatus, errorThrown){
+			        errorCallback(XMLHttpRequest, textStatus, errorThrown)
+			    }
+			})
+		}
+
+
+		this.search_people = function (search_query) {
+			$(".friends_search_results").html(
+				'<li class="center-align no-border"><div class="preloader-wrapper small active">'+
+			    '<div class="spinner-layer spinner-red-only">'+
+			      '<div class="circle-clipper left">'+
+			        '<div class="circle"></div>'+
+			      '</div><div class="gap-patch">'+
+			        '<div class="circle"></div>'+
+			      '</div><div class="circle-clipper right">'+
+			        '<div class="circle"></div>'+
+			      '</div>'+
+			    '</div>'+
+			  '</div></li>'
+			)
+			var formData = new FormData()
+			formData.append('search_query', search_query)
+			formData.append('key', 'find_friends')
+			this.makeUse(formData, 'find-friend', function (resp) {
+				if (resp.status != '0' && resp.status != 'empty') {
+					var output = ''
+					for (var i = 0; i < resp.entry.length; i++) {
+						var hook = resp.entry[i]
+						output += '<li class="col-md-12 no-p">'+
+            							'<a href="" class="friends_list_a clearfix">'+
+							                '<div class="pull-left">'+
+							                    '<img src="../icon.png" class="friends_list_img">'+
+							                    '<span class="friends_list_name">'+hook.surname+' '+hook.firstname+'</span>'+
+							                '</div>'+
+							                '<button class="btn btn-sm pull-right">add</button><i class="ti-angle-right pull-right friends_list_i"></i>'+
+							            '</a>'+
+							        '</li>'
+					}
+
+					
+				} else {
+					output = '<li class="col-md-12 no-p no-border">'+
+							'<h4 class="center-align">No result found...</h4>'+
+				        '</li>'
+				}
+
+				$(".friends_search_results").html(output)
+			}, function (a, b, c) {
+				console.log(a)
+			})
+		}
+	}
+	
+
+
+	var LC = new LC()
+
+	
+
+	$(".hundred").css({
+		'min-height': $(window).height()
+	})
+
+
+	$(".height-stay").css({
+		'height': $(window).height()-157
+	})
+
+	$(".scan-btn").on('click', function () {
+		
+		cordova.plugins.barcodeScanner.scan(
+	      	function (result) {
+	          	alert("We got a barcode\n" +
+	                "Result: " + result.text + "\n" +
+	                "Format: " + result.format + "\n" +
+	                "Cancelled: " + result.cancelled
+	            );
+	      	},
+	      	function (error) {
+	          	alert("Scanning failed: " + error);
+	      	}
+	   	)
+
+	})
+
+	
+
+})
